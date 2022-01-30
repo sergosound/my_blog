2346ea629cf29d55a9f979d77dd00d1e7c6844fb
sergey.dirko <sergey.dirko@auroraglobal.com>
2022-01-31 01:42:42 +0200

    add connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const createServerWithEnv = require("./utils/createServerWithEnv");
+const connectToDB = require("./utils/connectToDB");
 const { PORT } = require("./utils/constants");
 
 const logger = () =>
@@ -13,6 +14,7 @@ const logger = () =>
   try {
     const server = await createServerWithEnv(app, httpServer);
     await server.listen(PORT, logger);
+    await connectToDB();
   } catch (error) {
     console.log(error);
   }
--- server/utils/connectToDB.js
+++ server/utils/connectToDB.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
-const Realm = require("realm-web");
-
-const APP_ID = "<Your App ID>";
-const graphqlUri = `https://realm.mongodb.com/api/client/v2.0/app/${APP_ID}/graphql`;
 
 async function connectToDB() {
   try {
     await mongoose.connect(
-      "mongodb+srv://sergeydirko:<sergeydev2019>@cluster0.m1r5e.mongodb.net/todos",
+      "mongodb+srv://admin:admin@cluster0.m1r5e.mongodb.net/my_blog",
       {
         useNewUrlParser: true,
-        useFindAndModify: false,
-      }
+        // useFindAndModify: false,
+      },
     );
-    // app.listen(())
-  } catch (error) {}
+  } catch (error) {
+    throw new Error(error);
+  }
 }
+
+module.exports = connectToDB;
